cf7ba4330bfac507e85bdf67a744c754f39a594b
TsungHuaLee <qaz10130166@gmail.com>
2020-08-10 20:28:38 +0800

    git init

---
+++ conf.py
@@ -0,0 +1,5 @@
+MODEL_PATH = '/stage1_seg.pth'
+# CLSMODEL_PATH = '/home/tsung1271232/Pneumothorax-Detection/stage1_seg.pth'
+CLS_THRESHOLD = 0.3
+INPUT_FOLDER = '/inputs'
+OUTPUT_FOLDER = '/outputs'
